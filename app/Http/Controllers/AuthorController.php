5fc41693dffa025bafda152643c5d17463fa1dcb
FahmiSI01 <fahmifadillah804.com>
2026-04-24 20:55:45 +0700

    Tugas 5 Laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
@@ -54,5 +55,90 @@ class AuthorController extends Controller
             "message" => "Resource added successfully",
             "data" => $author
         ], 201);
+    }
+
+    public function show(string $id){
+        $author = Author::find($id);
+
+        if(!$author){
+            return response()->json([
+                "success" => false,
+                "message" => "Resource Not Found",
+            ], 404);
+        }
+
+        return response()->json([
+            "success" => true,
+            "data" => $author
+        ], 200);
+    }
+
+    public function update(Request $request, string $id){
+        $author = Author::find($id);
+
+        if(!$author){
+            return response()->json([
+                "success" => false,
+                "message" => "Resource Not Found",
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'photo' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'bio' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+        $data = [
+            'name' => $request->name,
+            'bio' => $request->bio,
+        ];
+
+        if($request->hasFile('photo')){
+            $image = $request->file('photo');
+            $image->store('authors', 'public');
+
+            if($author->photo){
+                Storage::disk('public')->delete('authors/'.$author->photo);
+            }
+
+            $data['photo'] = $image->hashName();
+        }
+
+        $author->update($data);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Resource updated successfully",
+            "data" => $author
+        ], 200);
+    }
+
+    public function destroy(string $id){
+        $author = Author::find($id);
+
+        if(!$author){
+            return response()->json([
+                "success" => false,
+                "message" => "Resource Not Found",
+            ], 404);
+        }
+
+        if($author->photo){
+            Storage::disk('public')->delete('authors/'.$author->photo);
+        }
+
+        $author->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Delete resource successfully",
+        ], 200);
     }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -50,4 +50,72 @@ class GenreController extends Controller
             "data" => $genre
         ], 201);
     }
+
+    public function show(string $id){
+        $genre = Genre::find($id);
+
+        if(!$genre){
+            return response()->json([
+                "success" => false,
+                "message" => "Resource Not Found",
+            ], 404);
+        }
+
+        return response()->json([
+            "success" => true,
+            "data" => $genre
+        ], 200);
+    }
+
+    public function update(Request $request, string $id){
+        $genre = Genre::find($id);
+
+        if(!$genre){
+            return response()->json([
+                "success" => false,
+                "message" => "Resource Not Found",
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+
+        $genre->update([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Resource updated successfully",
+            "data" => $genre
+        ], 200);
+    }
+
+    public function destroy(string $id){
+        $genre = Genre::find($id);
+
+        if(!$genre){
+            return response()->json([
+                "success" => false,
+                "message" => "Resource Not Found",
+            ], 404);
+        }
+
+        $genre->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Delete resource successfully",
+        ], 200);
+    }
 }
