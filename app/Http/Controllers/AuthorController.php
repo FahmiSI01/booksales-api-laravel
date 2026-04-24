9f97cdd257a4d90518c9c8a1c90d19f8f7d87c07
FahmiSI01 <fahmifadillah804.com>
2026-04-24 09:31:26 +0700

    Tugas 4 Laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,16 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
-        public function index(){
+    public function index(){
         $authors = Author::all();
+
+        if($authors->isEmpty()){
+            return response()->json([
+                "success" => true,
+                "message" => "No Resources Found",
+            ], 200);
+        }
 
         return response()->json([
             "success" => true,
             "message" => "Get All Resources",
             "data" => $authors
         ], 200);
+    }
+
+    public function store(Request $request){
+        // 1. Validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'bio' => 'required|string',
+        ]);
+        // 2. check validator eror
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+        // 3. upload image
+        $image = $request->file('photo');
+        $image->store('authors', 'public');
+        // 4. insert data
+        $author = Author::create([
+            'name' => $request->name,
+            'photo' => $image->hashName(),
+            'bio' => $request->bio,
+        ]);
+        // 5. response
+        return response()->json([
+            "success" => true,
+            "message" => "Resource added successfully",
+            "data" => $author
+        ], 201);
     }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 
 class BookController extends Controller
 {
     public function index(){
         $books = Book::all();
+
+        if($books->isEmpty()){
+            return response()->json([
+                "success" => true,
+                "message" => "No Resources Found",
+            ], 200);
+        }
 
         return response()->json([
             "success" => true,
@@ -16,4 +25,48 @@ class BookController extends Controller
             "data" => $books
         ], 200);
     }
+
+    public function store (Request $request){ {
+        // 1. Validator
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:100',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'cover_photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'genre_id' => 'required|exists:genres,id',
+            'author_id' => 'required|exists:authors,id',
+        ]);
+
+        // 2. check validator eror
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+
+        // 3. upload image
+        $image = $request->file('cover_photo');
+        $image ->store('books', 'public');
+
+        // 4. insert data
+        $book = Book::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+            'cover_photo' => $image->hashName(),
+            'genre_id' => $request->genre_id,
+            'author_id' => $request->author_id,
+        ]);
+
+        // 5. response
+        return response()->json([
+            "success" => true,
+            "message" => "Resource added successfully",
+            "data" => $book
+        ], 201);
+    }
+}
 }
